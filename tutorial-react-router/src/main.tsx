9e6cf919011ce11032c02b180920ed4aea7a5653
Gerson Klauck <gk120hf@gmail.com>
2025-05-13 10:10:27 -0300

    Nested routes

--- tutorial-react-router/src/main.tsx
+++ tutorial-react-router/src/main.tsx
@@ -1,12 +1,22 @@
 
 
 
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
+
+//import React from "react";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import Expenses from "./routes/Expenses/index.tsx";
+import Invoices from "./routes/Invoices/index.tsx";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Route>
+    </Routes>
   </BrowserRouter>
 )
