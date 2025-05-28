395399baf75347e2a374c04b15bbeb2fa2cce464
Gerson Klauck <gk120hf@gmail.com>
2025-05-28 12:04:53 -0300

    App.tsx; react 18

--- dscommerce-frontend/src/App.tsx
+++ dscommerce-frontend/src/App.tsx
@@ -13,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<ClientHome />}>
           <Route index element={<Catalog />} />
+          <Route path="catalog" element={<Catalog />} />
           <Route path="product-details" element={<ProductDetails />} />
         </Route>
       </Routes>    
