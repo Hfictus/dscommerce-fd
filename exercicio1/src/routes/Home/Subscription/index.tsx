3ce799af1e214b6f60ff41a8e122b3255a7b65cc
Gerson Klauck <gk120hf@gmail.com>
2025-06-06 12:42:38 -0300

    Ex1 end

--- exercicio1/src/routes/Home/Subscription/index.tsx
+++ exercicio1/src/routes/Home/Subscription/index.tsx
@@ -1,19 +1,17 @@
 
 
 
-
-
 import { Link } from "react-router-dom";
 import Button from "../../../components/Button";
 import Card from "../../../components/Card";
 
-export default function HomeBody() {
+export default function Subscription() {
     return(
-       <main>
+        <main>
             <section>
                 <div className="container">
                     <div className="mt30 mb30">
-                        <Card title="Página inicial" />
+                        <Card title="Página de inscrição" />
                     </div>
                     <div className="dflex">
                         <Link to="/promotion">
@@ -22,6 +20,6 @@ export default function HomeBody() {
                     </div>
                 </div>
             </section>
-       </main> 
+        </main>
     );
 }
