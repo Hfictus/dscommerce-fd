5740d92e75d6b8db82d0d3fb4ea2d00dc7a48d57
Gerson Klauck <gk120hf@gmail.com>
2025-06-06 11:58:57 -0300

    Layout

---
+++ exercicio1/src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+
+
+
+import './styles.css';
+
+type Props = {
+    text : string;
+}
+
+export default function Button({text} : Props) {
+    return(
+        <div className="button">
+            {text}
+        </div>
+    );
+}
---
+++ exercicio1/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+
+import './styles.css';
+
+export default function Header() {
+    return(
+        <header>
+            <div className="container">
+                <h1 className="site-name">MeuSite</h1>
+            </div>
+        </header>        
+    );
+}
